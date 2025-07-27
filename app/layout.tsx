71b1d11d6603bb5924e29d6453f41fdbf0128281
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-07-26 23:27:11 -0400

    move favicon to /public, add webmanifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,21 +19,22 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon_io/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon_io/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon_io/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
